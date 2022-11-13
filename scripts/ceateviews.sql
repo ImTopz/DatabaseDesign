a1c3b18015aecec1cfef5231efef992903cad7d1
张德东 <1976521709@qq.com>
2022-11-13 21:23:30 +0800

    Pics:Add new

---
+++ scripts/ceateviews.sql
@@ -0,0 +1,31 @@
+USE BookManageOS
+DROP VIEW IF   EXISTS checkPress 
+GO
+DROP VIEW IF EXISTS checkDepart
+GO
+DROP VIEW IF EXISTS checkBorrowStatus
+GO
+
+CREATE VIEW checkDepart AS
+	SELECT  Ename,Dname FROM Employee e inner join Department d ON e.Dno = d.Dno
+GO
+CREATE VIEW checkPress AS
+	SELECT Bname,p.Pname FROM Books b inner JOIN Press p ON b.Pname = p.Pname
+GO
+CREATE VIEW checkBorrowStatus AS 
+	SELECT Ename,Bname,LendingDate,ShouldpaybackDate,RealpaybackDate
+		FROM Employee  e join Borrow  b ON e.Eno = b.Eno  join Books on b.Bno=Books.Bno
+GO
+CREATE VIEW checkBorrowNumber AS
+	SELECT Ename,
+GO
+
+
+
+SELECT * FROM checkDepart
+GO
+SELECT * FROM checkPress
+GO
+SELECT* FROM checkBorrowStatus
+GO
+
--- scripts/createTables.sql
+++ scripts/createTables.sql
@@ -1,13 +1,13 @@
 use BookManageOS
 go
-
+--�������ݱ�
 if exists(select * from sysobjects where name='Press')
 drop table Press
 go
 create table Press
 (
 Pname     varchar(50)  PRIMARY KEY,
-Paddress  varchar(255) null,
+Paddress  varchar(255) unique,
 Phone     varchar(50) null,
 Pcontact  varchar(255) 
 )
@@ -18,8 +18,8 @@ drop table Books
 go
 create table Books 
 (
-Bname   varchar(50)  PRIMARY KEY,
-Bno     varchar(255) not null,
+Bno   varchar(13)  PRIMARY KEY,
+Bname     varchar(50) not null,
 Bauthor varchar(50) null,
 Bprice  float   not null,
 Bnumber int     null,
@@ -33,7 +33,7 @@ drop table Department
 go
 create table Department
 (
-Dno    varchar(20) PRIMARY KEY,
+Dno    int PRIMARY KEY,
 Dname  varchar(100),
 Dphone varchar(100)
 )
@@ -44,11 +44,11 @@ drop table Employee
 go
 create table Employee
 (
-Eno    varchar(50) PRIMARY KEY ,
+Eno    varchar(12) PRIMARY KEY ,
 Ename  varchar(100) ,
 Esex   nchar(1) CHECK(Esex='��' OR Esex = 'Ů'),
 Ebirth date,
-Dno    varchar(20) FOREIGN KEY REFERENCES Department(Dno)
+Dno    int FOREIGN KEY REFERENCES Department(Dno)
 )
 go
 
@@ -58,16 +58,12 @@ drop table Borrow
 go
 create table Borrow
 (
-Bno  varchar(100) ,
-Eno  varchar(100) ,
+Bno  varchar(13),
+Eno  varchar(12),
 LendingDate date,
 ShouldpaybackDate date,
-RealpaybackDate date
+RealpaybackDate date,
 PRIMARY KEY(Bno,Eno)
 )
 go
-
- 
-
-
 
